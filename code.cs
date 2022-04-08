e5fc18d059edbfead08faa967ed5d81d82e6ee25
rishabh4614 <rishabhr00j@gmail.com>
2022-04-08 13:36:47 +0530

    first commit

---
+++ code.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the name of User: ");
+            string Name = Convert.ToString(Console.ReadLine());
+
+            if (Name == "Rishabh")
+            {
+                Console.WriteLine("Hello Mr " + "Rishabh");
+                Console.WriteLine("Welcome to Zensar Family");
+            }
+        }
+    }
+}
